0acf1dcad14551f6ae9d205cddc08bab6ea0e5f9
Silviana <125959967+silviana1201@users.noreply.github.com>
2023-02-26 09:30:14 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,102 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>website.</title>
+    <link rel="stylesheet" href="style.css">
+
+</head>
+<body>
+    <nav>
+        <div class="wrapper">
+          
+            <div class="logo"><a href=''>Silviaa</a></div>
+            <div class="menu">
+                <ul>
+                    <li><a href ="#Home">Home</a></li>
+                    <li><a href ="#courses">courses</a></li>
+                    <li><a href ="#profile">profile</a></li>
+                    <li><a href ="#Contact">Contact</a></li>
+                     
+                    
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <div class="wrapper">
+
+    </section>
+     <!--untuk home-->
+        <section id="Home">
+            <img src="https://img.freepik.com/free-vector/colourful-illustration-programmer-working_23-2148281410.jpg?size=338&ext=jpg&ga=GA1.2.391025562.1677137913&semt=sph"/>
+            <div class="kolom">
+                <p class="deskripsi">Belajar programming # Studi Independent</p>
+                <h2>yokk bisa, Tetap semangat</h2>
+                <p>belajar memang melelahkan,namun akan lebih melelahkan lagi bila saai ini kamu tidak belajar.“Education is not the learning of facts, but the training of the mind to think. Struggle that you do today is the single way to build a better future.” – Albert Einstein</p>
+               
+
+            </section>
+
+            <!--untuk courses-->
+            <section id="courses">
+               <div class="kolom">
+                   
+                   <h2>Online Courses</h2>
+                   <p> course Online Terbaik dengan biaya terjangkau dan fasilitas lengkap.Pembelajaran Fokus Pada Praktek,Bersetifikat,Berkualitas & Terjangkau.course ini dapat membantu kalian belajar kapanpun dan dimanapun. Seluruh materi disusun dengan sangat detail dan sesuai dengan kebutuhan industri. sehingga setelah mengikuti course ini kamu bisa mendapatkan pekerjaan sebagai Enginer IT dengan sanngat mudah.</p>
+                
+                  
+               </div>
+               <img src="https://t3.ftcdn.net/jpg/03/37/42/64/240_F_337426455_OV5RJfk9rvJ0Ky1Imlc6335DqpmTQSle.jpg"/>
+            </section>
+            <!--untuk profile-->
+            <section id="profile">
+                <div class="tengah">
+                    <div class="kolom">
+                        <p class="deskripsi">My Profile</p>
+                        <h2>Profile</h2>
+                        <p> Foto Ini Saya Ambil Dari Google Yakk.</p>
+                    </div>
+
+                    <div class="profile">
+                        <div class="kartu-profile">
+
+                            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ-NRvrhUgPrEXyPDIutEaLYIC-Nca5Oa3O7gDQAAbwohbTnXAGiZDRrtGqoKxX4pW9u1k&usqp=CAU"/>
+                            <p>Silviaa</p>
+                        </div>
+                    </div>
+                </div>
+            </section>
+          </div>
+             <div id="contact">
+        <div class="wrapper">
+            <div class="footer">
+                <div class="footer-section">
+                    <h3>silviaa.</h3>
+                    <p>Salam Kenal Guysss Ehehehe </p>
+                </div>
+                    <div class="footer-section">
+                        <h3>About</h3>
+                        <p>Hollaa Teman-Teman Saya Silviana Mahasiswa Semester 6 Prodi Sistem Informasi Asal Kampus Universitas Teknologi Mataram</p>
+                    </div>
+                     <div class="footer-section">
+                        <h3>Contact</h3>
+                        <p>Jl. Pramuka Kuripan Utara Lombok Barat</p>
+                        <p> kode pos: 38862</p>
+                    </div>
+                      <div class="footer-section">
+                        <h3>Social</h3>
+                        <p><b>Instagram: </b>Silviaa___</p>
+                    </div>
+                </div>
+             </div>
+       </div>
+
+    <div id="copyright">
+        <div class="wrapper">
+            &copy; 2023. <b>silviaa.</b> All  Rights Reserved.
+        </div>
+    </div>
+</body>
+</html>
